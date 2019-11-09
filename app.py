712bf85b4f58e5e3e5cb4bd840934052a93d4716
Surya Kant Bansal <suryakantbansal97@gmail.com>
2019-11-09 18:09:33 +0530

    random meme flask app

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import os
+import random
+
 from flask import Flask, render_template
 
 from meme import MemeEngine
-# @TODO Import your Ingestor and MemeEngine classes
+from ingestors import Ingestor
 
 app = Flask(__name__)
 
@@ -10,39 +13,29 @@ meme = MemeEngine('./static')
 
 def setup():
     """ Load all resources """
-
     quote_files = ['./_data/DogQuotes/DogQuotesTXT.txt',
                    './_data/DogQuotes/DogQuotesDOCX.docx',
                    './_data/DogQuotes/DogQuotesPDF.pdf',
                    './_data/DogQuotes/DogQuotesCSV.csv']
-
-    # TODO: Use the Ingestor class to parse all files in the
-    # quote_files variable
-    quotes = None
+    all_quotes = []
+    for f in quote_files:
+        all_quotes.extend(Ingestor.parse(f))
 
     images_path = "./_data/Photos/Dog/"
-
-    # TODO: Use the pythons standard library os class to find all
-    # images within the images images_path directory
-    imgs = None
-
-    return quotes, imgs
+    all_images = []
+    for root, dirs, files in os.walk(images_path):
+        all_images = [os.path.join(root, name) for name in files]
+    return all_quotes, all_images
 
 
-quotes, imgs = setup()
+quotes, images = setup()
 
 
 @app.route('/')
 def meme_rand():
     """ Generate a random meme """
-
-    # @TODO:
-    # Use the random python standard library class to:
-    # 1. select a random image from imgs array
-    # 2. select a random quote from the quotes array
-
-    img = None
-    quote = None
+    img = random.choice(images)
+    quote = random.choice(quotes)
     path = meme.make_meme(img, quote.body, quote.author)
     return render_template('meme.html', path=path)
 
--- meme/meme_engine.py
+++ meme/meme_engine.py
@@ -5,14 +5,19 @@ from PIL import Image, ImageFont, ImageDraw
 
 
 class MemeEngine:
+    """ The engine behind meme generation """
     def __init__(self, output_dir):
+        """ Save and create the output directory path """
         self.output_dir = output_dir
+        self.count = 1
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
 
     def make_meme(self, img_path, text, author, width=500):
+        """ Create a meme using the given image, text and author """
         img = Image.open(img_path)
-        outfile = os.path.join(self.output_dir, "temp.jpg")
+        outfile = os.path.join(self.output_dir, f"temp-{self.count}.jpg")
+        self.count += 1
         real_height, real_width = img.size
         height = int(real_height * width / real_width)
         img.thumbnail((height, width))
