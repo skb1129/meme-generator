43213f35c07bbbccfc58ed5bc141371e46dab076
Surya Kant Bansal <suryakantbansal97@gmail.com>
2020-06-07 12:40:22 +0530

    docker container setup

--- ingestors/pdf_ingestor.py
+++ ingestors/pdf_ingestor.py
@@ -9,7 +9,7 @@ class PDFIngestor(IngestorInterface):
     @classmethod
     def parse(cls, path):
         text_file = './pdf_to_text.txt'
-        cmd = f"pdftotext -layout -nopgbrk {path} {text_file}"
+        cmd = f"./pdftotext -layout -nopgbrk {path} {text_file}"
         subprocess.call(cmd, shell=True, stderr=subprocess.STDOUT)
         quotes = TextIngestor.parse(text_file)
         os.remove(text_file)
